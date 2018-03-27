e400e90eac004e759e45810b12e89d276727c62c
shettar20 <shettar20@gmail.com>
2018-03-27 06:37:16 +0530

    GoogleCodeJam application setup

---
+++ WorldFInals-2017/src/com/google/code/jam/DiceStraightMain.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.google.code.jam;
+
+/**
+ * @author Pradeep.Shettar
+ *
+ */
+public class DiceStraightMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
